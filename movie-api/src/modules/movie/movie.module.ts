b33517f3a8d54057137e57c94d244493032f37a7
nthuanreal <nthuan2609@gmail.com>
2025-01-14 10:06:14 +0700

    patch "forwardRef not found"

--- movie-api/src/modules/movie/movie.module.ts
+++ movie-api/src/modules/movie/movie.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MovieService } from './movie.service';
 import { MovieSchema } from './schemas/movie.schema';
